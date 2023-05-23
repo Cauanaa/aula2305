1426e633e0a408790d02870e2070e1ea35bb51d3
Cauanaa <114515736+Cauanaa@users.noreply.github.com>
2023-05-23 17:23:15 -0300

    Add files via upload

---
+++ aula16-05-main/pweb1_aula_php-main/index.php
@@ -0,0 +1,8 @@
+<?php
+include "estrutura/cabecalho.inc.php";
+include "estrutura/menu.inc.php";
+include "estrutura/paginas/conteudo.inc.php";
+include "estrutura/paginas/data.php";
+include "estrutura/paginas/exemplos_introducao.php";
+include "estrutura/rodape.inc.php";
+?>
---
+++ aula16-05-main/pweb1_aula_php-main/paginas/login.php
@@ -0,0 +1,45 @@
+<?php
+include "../BD.class.php";
+ 
+$conn = new BD();
+
+if(!empty($_POST)){
+    session_start();
+
+    $usuario = $conn->login($_POST);
+
+    if (
+        $_POST["login"] == $usuario->login && 
+        $_POST["senha"] == $usuario->senha
+    ) {
+        $_SESSION["login"] = $_POST['login'];
+        $_SESSION["senha"] = $_POST['senha'];
+        
+        header("location: main.php");
+    }
+
+} else {
+    header("location: login.php?msg=Erro");
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>Sistema Acadêmico</h3>
+    <form action="login.php" method="post">
+        <label>Login</label>
+        <input type="text" name="login" />
+        <label>Senha</label>
+        <input type="password" name="senha" />
+        <button type="submit">Logar</button>
+</form>
+</body>
+</html>
